4ee50f320d25fab331e76975cc1503bfca8b7a7d
VPS Backup Agent <backup@major-tour.com>
2026-09-16 22:00:03 +0000

    Automated backup: 2026-09-16 22:00:03

--- db_backup/db_dump.sql
+++ db_backup/db_dump.sql
@@ -2,7 +2,7 @@
 -- PostgreSQL database dump
 --
 
-\restrict JrLkVVHqcX9IYJm3dgkDddhFoxGGcjlAYcJqRqJuT1tU9p3zrpkRM0jfQtboCxB
+\restrict i8GUq0Cjow3ROBtM8gCTjg0Ra2gf6e1QyqrXZ7Dzag94NxDaqwp4RR12ToByCQZ
 
 -- Dumped from database version 16.15
 -- Dumped by pg_dump version 16.15
@@ -889,5 +889,5 @@ REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 -- PostgreSQL database dump complete
 --
 
-\unrestrict JrLkVVHqcX9IYJm3dgkDddhFoxGGcjlAYcJqRqJuT1tU9p3zrpkRM0jfQtboCxB
+\unrestrict i8GUq0Cjow3ROBtM8gCTjg0Ra2gf6e1QyqrXZ7Dzag94NxDaqwp4RR12ToByCQZ
 
